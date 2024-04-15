51bc3f46e0aee3f82e311d1271087f54534a6cad
falulu45 <106511529+falulu45@users.noreply.github.com>
2024-04-14 23:11:29 -0400

    first push

---
+++ src/app/mi-rayen/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <h1>AQUI VA LAS STATS</h1>
+        <button className=" text-red-400 shadow-xl">click</button>
+    </div>
+    
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,84 @@
 import Image from "next/image";
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {Card, CardBody, Button, Slider} from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
+    <div className=" pt-32 h-[100%]" id="inicio">
+      <div className=" m-auto w-[350px] xl:w-[500px]">
+      <Card
+        isBlurred
+        className=" animate-pulse p-0 border-none bg-background/60 dark:bg-default-100/50 w-[160px] mb-3 mr-6 ml-auto"
+        shadow="none"
+        >
+          <CardBody className=" text-center p-0 max-w-[160px] m-auto">
+            <h2 className=" text-center mt-1 text-small pb-1"><span className=" text-green-500">cuidemos</span> nuestro aire</h2>     
+          </CardBody>
+        </Card>
+        <Card
+        isBlurred
+        className=" p-0 border-none bg-background/60 dark:bg-default-100/50"
+        shadow="sm"
+        >
+          <CardBody className=" text-center p-14">
+            <Image 
+              src="/img/logo.png"
+              height={100}
               width={100}
-              height={24}
-              priority
+              alt="logo"
+              className="m-auto w-[5rem] mb-[-0.5rem] xl:w-[8rem]"
             />
-          </a>
+            <h1 className=""><span className=" font-black text-5xl sm:text-7xl">Rayen</span><span className="font-black text-5xl text-green-500 sm:text-7xl">CO2</span></h1>
+          </CardBody>
+        </Card>
+        <Card
+        isBlurred
+        className="animate-pulse p-0 border-none bg-background/60 dark:bg-default-100/50 w-[180px] mt-3 ml-6"
+        shadow="none"
+        >
+          <CardBody className=" text-center p-0 max-w-[160px] m-auto">
+            <h2 className=" text-center text-small pb-1 mt-1 mb-0"> por un <span className=" text-green-500">futuro</span> sostenible</h2>     
+          </CardBody>
+        </Card>
+      </div>
+      <div className=" ">
+        <div id="objetivo" className="w-[350px] m-auto mt-[10rem] pt-10 content-center flex md:w-[900px]">
+        <Card
+          isBlurred
+          className=" p-0 border-none bg-background/60 dark:bg-default-100/50 invisible md:visible md:mr-5 w-0 md:w-[40%]"
+          shadow="sm"
+          >
+            <CardBody className=" text-center p-14">
+              <Image 
+                src="/img/logo2-bn.png"
+                height={200}
+                width={200}
+                alt="logo-bn"
+                className="m-auto invert opacity-80"
+              />
+            </CardBody>
+        </Card>
+        <Card
+        isBlurred
+        className="p-0 border-none bg-background/60 dark:bg-default-100/50 min-w-[350px] m-auto md:w-[55%]"
+        shadow="sm"
+        >
+          <CardBody className=" p-5">
+          <h1 className=" font-sans text-2xl">Objetivos</h1>
+          <p className=" text-justify">Lo que nos llevó a crear RayenCO2 es la necesidad de dispositvos accesibles y amigables que detecten gases nocivos
+            para nuestro cuerpo, como el humo de cigarros, gas licuado, gas metano y monoxido de carbono. <br/> Ah visto mapas de co2, contaminación? Porque no mostrar 
+            esta información de manera fácil y de libre acceso, RayenCO2 ofrece un <a href="#mapa">mapa</a> que muestra la calidad del aire en relación a las párticulas por millón de gases nocivos en el ambiente
+          </p>     
+          </CardBody>
+        </Card>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <p className=" pt-96" id="mapa">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem vitae quae perspiciatis labore ducimus recusandae fuga, molestias obcaecati impedit beatae, unde est nisi. Voluptate placeat quos, corrupti perferendis voluptatum ab.
+      At ullam doloribus debitis eveniet vitae. Quo, voluptatibus, nihil harum veritatis rerum temporibus necessitatibus pariatur accusamus ipsum ipsa consequuntur consequatur quae minima quisquam asperiores? Vel vitae eum quae non autem?
+      Culpa ullam deleniti dolores blanditiis id repudiandae unde quaerat voluptatem sint maxime. Dolorem quasi aut nesciunt repellat nisi. Rerum atque quae totam adipisci eum voluptatum nostrum pariatur maiores quaerat soluta!
+      Sint consequatur nam ullam, et dignissimos quo harum cumque exercitationem similique ratione! Ipsum porro eaque id facere rerum, consequuntur alias minima vero. Similique, reprehenderit voluptatum animi quibusdam libero debitis nobis.
+      Fugiat sed non similique dolores, exercitationem repellat dolore temporibus repudiandae facere quae, maiores harum incidunt illo animi consequatur nobis nulla blanditiis ex possimus a, consectetur quis qui. Voluptatibus, nihil omnis?</p>
+    </div>
   );
 }
